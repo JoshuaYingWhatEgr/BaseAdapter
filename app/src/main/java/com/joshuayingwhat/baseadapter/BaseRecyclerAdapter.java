9018ef0f9d548109e8aa1a54167cfd5f9f85ed7b
joshuayingwhat <17721354011@163.com>
2019-08-29 10:32:49 +0800

    设置布局和滚动时不加载数据

--- app/src/main/java/com/joshuayingwhat/baseadapter/BaseRecyclerAdapter.java
+++ app/src/main/java/com/joshuayingwhat/baseadapter/BaseRecyclerAdapter.java
@@ -1,5 +1,7 @@
 package com.joshuayingwhat.baseadapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -14,8 +16,24 @@ import java.util.List;
  */
 public abstract class BaseRecyclerAdapter<T> extends RecyclerView.Adapter<BaseRecyclerHolder> {
 
+    /**
+     * recyclerview的布局
+     */
     private int resourceLayout;
+
+    /**
+     * 初始化数据
+     */
     private List<T> resDatas;
+
+    /**
+     * 是否在滑动中
+     * true 不在滑动
+     * false 滑动
+     */
+    private boolean isScrolling;
+
+    private final Context viewContext;
 
     public BaseRecyclerAdapter(Collection<T> resDatas, RecyclerView recyclerView, int resourceLayout) {
         if (resDatas == null) {
@@ -28,18 +46,36 @@ public abstract class BaseRecyclerAdapter<T> extends RecyclerView.Adapter<BaseRe
 
         this.resourceLayout = resourceLayout;
 
+        viewContext = recyclerView.getContext();
 
+        /**
+         * 这里是设置recyclerview滚动的时候不加载图片
+         */
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                //recyclerview不在滑动
+                int scrollStateIdle = RecyclerView.SCROLL_STATE_IDLE;
+                isScrolling = (newState == scrollStateIdle);
+                if (isScrolling) {
+                    //更新recyclerview所有数据
+                    notifyDataSetChanged();
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return resDatas == null ? 0 : resDatas.size();
     }
 
     @NonNull
     @Override
     public BaseRecyclerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new BaseRecyclerHolder(LayoutInflater.from(viewContext)
+                .inflate(resourceLayout, parent, false));
     }
 
     @Override
